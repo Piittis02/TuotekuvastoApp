062807af0bb15ad7044ccc47850d3d0546c1f0a8
Leevi Piittala <Leevi.Piittala@edu.karelia.fi>
2025-01-21 12:41:52 +0200

    First commit

---
+++ KuvastoApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using KuvastoApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace KuvastoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Product()
+
+        {
+
+            var products = GetProducts();
+
+            return View(products);
+
+        }
+
+        private List<Product> GetProducts()
+
+        {
+
+            var json = System.IO.File.ReadAllText("wwwroot/products.json");
+
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+
+        }
+    }
+}
---
+++ KuvastoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KuvastoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
